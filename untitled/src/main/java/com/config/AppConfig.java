d40bad66893504648983eda3402bf222816ffc48
S M Tusher <85401497+smtusher007@users.noreply.github.com>
2022-12-27 10:20:13 +0600

    Config

---
+++ untitled/src/main/java/com/config/AppConfig.java
@@ -0,0 +1,71 @@
+package com.config;
+
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"com.repository","com.service","com.controller","com.domain","com.exception"})
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css");
+    }
+
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/views");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return  internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource()
+    {
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setDatabaseName("tm");
+        mysqlDataSource.setURL("jdbc:mysql://localhost:3306/tm");
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("root");
+        return mysqlDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory()
+    {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.domain");
+        Properties properties = new Properties();
+        sessionFactory.setHibernateProperties(properties);
+        return sessionFactory;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager()
+    {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+}
